8f747dae7279be88d02a02ddd6699ef236034f2c
kevin <luiscorreo.com>
2026-06-17 13:19:12 -0500

    feat: crear vista inicial de progreso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import AuthPage from './pages/AuthPage'
+import Progreso from './pages/Progreso';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/progreso" element={<Progreso />} />
       </Routes>
     </BrowserRouter>
   )
