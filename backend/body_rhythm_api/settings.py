352f4d1c30ef655b7ccae13ce92e9cafbde8569a
kevin <luiscorreo.com>
2026-08-23 20:01:28 -0500

    Fix CORS for frontend port

--- backend/body_rhythm_api/settings.py
+++ backend/body_rhythm_api/settings.py
@@ -129,6 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
 ]
 
 # REST FRAMEWORK
